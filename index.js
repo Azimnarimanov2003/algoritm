0bbb71016d5b295b43bbea1876cf3ba2ccea5a26
𝔹-𝕊𝕄𝔸ℝ𝕋 <azimnarimanov3@gmail.com>
2024-04-16 18:11:46 +0500

    add

---
+++ index.js
@@ -0,0 +1,431 @@
+// let yoshi=prompt("yoshingizni kiriting")
+// if(yoshi>20){
+//     console.log("xush kelibsiz ")
+// }
+// else if(yoshi<20) {
+//     console.log("hali yoshsiz")
+// }
+// let a=prompt("a tomonni kiriting")
+// let b=prompt("b tomonni kiriting")
+// let c=prompt("c tomonni kiriting")
+// if((a+b)>c&&(a+c)>b, (c+b)>a) {
+//     console.log("uchburchak hosil buladi")
+// }
+// else if((a+b)<c, (a+c)<b, (c+b)<a) {
+//     console.log("uchburchak hosil bulmaydi")
+// }
+// 4-masala
+// let a=prompt("a sonni kiritig")-0
+// if(a>0) {
+// 1-masala
+// console.log(Math.sqrt(a)) 
+// }
+// else {
+//     console.log("siz manfiy son kirittingiz")
+// }
+
+
+
+
+
+// 1-masala
+// let raqam = prompt('raqamni kiriting')
+// if(raqam%2 ==0 ) {
+//     console.log((raqam*raqam))
+// }
+// else{
+//     console.log(Math.sqrt(raqam))
+// }
+
+// 1-masala
+// let number = prompt("Raqam kiriting");
+
+// if ( number % 2 ===0 ) {
+//     console.log(number**2);
+// } else {
+//     console.log(Math.sqrt(number));
+// }
+// 3-masala
+// let matn = prompt('Matnni kiriting')
+// let sarlavha = prompt('Sarlavhani  kiriting')
+// if(matn.length>15){
+// console.log(matn.concat(' ',sarlavha))
+// }
+// else if(matn.length<15) {
+//     console.log(matn)
+// }
+// else{
+//     console.log(sarlavha)
+// }
+// 4-masala 
+
+// let x = prompt("raqamni kiriting")-0
+// if(x>0){
+//     console.log(x+1)
+// }
+// else{
+//     console.log(x-2)
+// }
+//  5-masala 
+// let a = prompt("a ni kiriting")
+// let b = prompt("b ni kiriting")
+// let c = prompt("c ni kiriting")
+//  let A = 0
+//  let B = 0
+//  let C = 0
+//  if(a>0)
+// {
+//     A++
+// }
+// else if (a<0)
+// {
+//     B++
+// }
+// else {
+//     C++
+// }
+
+// if(b>0)
+// {
+//     A++
+// }
+// else if (b<0)
+// {
+//     B++
+// }
+// else {
+//     C++
+// }
+// if(c>0)
+// {
+//     A++
+// }
+// else if (c<0)
+// {
+//     B++
+// }
+// else {
+//     C++
+// }
+// console.log( A, " ta musbat sonlar  ")
+// console.log( B,  " ta manfiy sonlar " )
+// console.log( C,   " ta  nollar ")
+// 5-masala 
+// let  x = prompt('x ni kiriting')
+// let  y = prompt('y ni kiriting')
+// let  z = prompt('z ni kiriting')
+  
+// if(x<y && x<z  ) {
+//     console.log( x, 'kichik son'  )
+// }
+// else if (y<x && y<z)
+// {
+//     console.log(y, 'kichik son' )
+// }
+// else {
+//     console.log(z, 'kichik son ' )
+// }
+  
+// 6-masala
+// let x = prompt(" x ni kiriting")
+// if(x<-2  &  x>2){
+//     console.log(2*x)
+// }
+// else{
+//     console.log(-3*x)
+// }
+  
+// 7-masala 
+// let x = prompt( 'x  kiriting')
+// if( x<0 && x%2==0){
+//     console.log("manfiy juft")
+// }
+// else if(x>0 && x%2==0)
+// {
+//     console.log("musbat juft ")
+// }
+// else if( x<0 && x%2==1){
+//     console.log("manfiy toq")
+// } 
+// else if(x>0 && x%2==1)
+// {
+//     console.log("musbat toq ")
+// }
+// else{
+//     console.log("noll")
+// }   
+
+// 1-masala
+// if1. Butun son berilgan. Agar, berilgan son musbat 
+// bo`lsa, 1 ga oshirilsin, aks holda o`zgartirilmasin.
+//  Hosil bo`lgan sonni ekranga chiqaruvchi programma tuzilsin.
+// let x = prompt('x ni kiriting ')-0
+// if(x>0) {
+//     console.log( ++x)
+
+// }
+// else {
+//     console.log( x)
+// }
+  
+
+// 2-masala
+// Butun son berilgan. Agar, berilgan son musbat 
+// bo`lsa, 1 ga oshiring, agar manfiy bo`lsa 2 ga kamaytiring.
+//  Agar 0 ga teng bo`lsa, 10 ni o`zlashtirsin.
+//  Hosil bo`lgan sonni ekranga chiqaruvchi programma tuzilsin.
+//  let x = prompt('x ni kiriting')
+//  if(x>0){
+//     console.log(++x)
+//  }
+//  else if(x<0){
+//     console.log(x-2)
+//  }
+//  else{
+//     console.log(x=10)
+//  } 
+
+// 3-masala 
+// . Uchta butun son berilgan. 
+// Shu sonlar orasidan nechta musbat 
+// son borligini aniqlovchi programma tuzilsin.
+// let a = prompt( 'a ni kiriting' )
+// let b = prompt( 'b ni kiriting' )
+// let c = prompt( 'c ni kiriting' )
+// let A = 0
+
+// if(a>0) {
+//     A++
+// }
+// if(b>0){
+//     A++
+// }
+// if(c>0) {
+//     A++
+// }
+// console.log( A, " ta musbat son")
+
+
+
+// 4-masalaa
+
+// Ikkita butun son berilgan.
+//  Shu sonlarning kattasini aniqlovchi programma tuzilsin.
+// let a = prompt('a ni kiriting')
+// let b = prompt('b ni kiriting')
+// if(a>b) {
+
+//     console.log( a)
+// }
+// else {
+//     console.log( b)
+// }
+
+  // For  1- misol
+//   For1. k va n butun sonlari berilgan (n > 0).
+//    k sonini n marta chiqaruvchi programma tuzilsin.
+// for(let k = 0; k <= 10; k++) 
+// console.log( k )
+
+// 2-misol 
+// For2. Bir kg konfetning narxi berilgan (haqiqiy son)
+// . 0.1, 0.2, …, 0.9, 1 kg konfetni narxini chiqaruvchi programma tuzilsin.
+  // for(let x = 0.1; x < 1;  x+=0.1)
+  // {
+  //   console.log( x )
+  // }
+
+
+
+  // 3-   misol 
+//   For3. n butun soni berilgan (n > 0).
+//    Quyidagi yig'indini hisoblovchi programma tuzilsin. 
+// S = n^2 +(n+1)^2+(n+2)^2 + ... +(2*n)^2
+
+
+// let Sum = 0; 
+// let n = prompt(' n ni kiriting:');
+// for(let  i = 0; i < 2 * n; i++){
+//   Sum += i * i;
+// }
+// console.log('Man sani ishladima ',Sum)
+
+// 4-misol
+// For4. N butun soni berilgan.
+//  Quyidagi yig'indini chiqaruvchi programma tuzilsin. 
+// 1^N+2^(N-1) + ...+ N^1
+// Input: N = 5;
+// 1 ^ 5 + 2 ^ 4 + 3 ^ 3 + 4 ^ 2 + 5 ^ 1
+// Output: 65
+// let yigindi = 0
+// let n = prompt( ' n ni kiriting :')
+// for( let i = 1; i <= n; i++ )
+// {
+//   yigindi += Math.pow(i, n - i + 1);
+
+// }
+// console.log(yigindi)
+
+// 5-misol 
+// Bir kg konfetning narxi berilgan
+//  (haqiqiy son). 1.2, 1.4, ..., 2
+//   kg konfetni narxini chiqaruvchi programma tuzilsin.
+
+// let i = 0
+// for( let i = 1 ;i < 2 ; i += 0.2 )
+// {
+//   console.log(i);
+// }
+//   6-misol
+// For5. a va b butun sonlari berilgan (a < b). a dan b gacha 
+// bo'lgan barcha butun sonlar yig'indisini chiqaruvchi programma tuzilsin.
+ 
+// let a = prompt('a  ni kiriting :')-0
+// let b = prompt(' b ni kiriting:')-0
+// let Yigindi = 0;
+// for(let i = a; i <= b; i++){
+//   Yigindi += i;
+// }
+// console.log('a  dan b gacha bulgan sonlar yigindisi' ,Yigindi);
+
+
+// 7-misol
+// let a = 7;
+// let b = 50;
+// for (let i = 8; i <= b; i += 2) {
+//   console.log(i);
+// }
+// 8-misol
+
+// let b = 100; 
+// let S = 0;
+// for( let i = 23; i <= b; i ++ ){
+// S += i;
+
+// }
+// console.log(S);
+ 
+// 9- misol
+
+// 11 dan 39 gacha bulgan sonlar ichidan 7 
+//ga qoldiqsiz bolinadigan sonlarning ko'paytmasini
+//hisoblash
+// let a = prompt('a ni kiriting :' );
+// let b = prompt('b ni kiriting :' );
+// let kupaytma = 1;
+// for( let i = a; i <= b; i ++) {
+//   if(i % 7 == 0) {
+//   kupaytma *= i;
+  
+// }
+
+// }
+
+
+
+// 2- variant
+// console.log(kupaytma);
+// let kopaytma = 1;
+// for (let son = 11; son <= 39; son++) {
+//     if (son % 7 === 0) {
+//         kopaytma *= son;
+//     }
+// }
+
+// console.log("Qoldiqsiz bo'linadigan sonlarning ko'paytmasi:", kopaytma);
+ // 9- misol
+
+// 1000 dan 2000 gacha sonlar ichidan 13 ga bulinadigan eng kichik sonni chiqaring
+  
+// let a = 1000;
+// let b = 2000;
+// let engKichikSon = Infinity;
+// for(let  i = a; i <= b; i ++){
+// if (i % 13 === 0  && i < engKichikSon) {
+//   engKichikSon = i
+// }
+// }
+// console.log("1000 dan 2000 gacha 13 ga qoldiqsiz bo'linadigan eng kichik son:", engKichikSon);
+
+
+
+// 10-misol
+// Bir kg konfetning narxi berilgan
+//  (haqiqiy son). 1, 2, 10 kg konfetni narxini chiqaruvchi programma tuzilsin.
+
+// let a = prompt('a ni kiriting :')
+// let S
+// for(let i = 1; i <= 10 ; i ++ ) {
+// S = i*a
+// console.log(S);
+// }
+// 11- misol
+// a va b butun sonlari berilgan (a < b). a dan b gacha bo'lgan
+//  barcha butun sonlar kupaytmasini chiqaruvchi programma tuzilsin.
+
+// let a = prompt('a ni kiriting :')-0
+// let b = prompt( 'b ni kiriting (a<b):')-0
+// let yigindi = 1
+// for( i = a; i <=b; i++){
+// yigindi *= i;
+// console.log(yigindi);
+
+// }
+
+// 12-misol
+
+
+// For11. n butun soni va
+//  a haqiqiy soni berilgan (n > 0). a ning
+//   n - darajasini aniqlovchi programma tuzilsin.
+// a^n=a*a*a...a;
+
+
+// let a = prompt('  a ni kiriting')
+// let n = prompt(' n ni kiriting')
+//   let natija = 1;
+//   for (let i = 0; i < n; i++) {
+//     natija *= a;
+//     console.log('a ning b chi darajasi : '  ,natija);
+//   }
+
+// 13-misol
+// . n butun soni va a haqiqiy soni berilgan (n > 0).
+//  Bir sikldan foydalanib quyidagi a ning 1 dan n gacha
+//   bo'lgan barcha darajalarini chiqaruvchi va yig'indini
+//    hisoblovchi programma tuzilsin. 
+// S = 1+a^1+a^2 + a^3 + ... a^n
+
+// let a = prompt(' a  ni kiriting :')
+// let n = prompt('n ni kiriting :')
+// let S = 0;
+// let natija = 1 ;
+// for( let i = 0; i < n; i++ ){
+// natija *= a;
+//   S += natija;
+//   console.log(S)
+// }
+
+// 14-misol
+// For18. Sonning barcha bo’luvchilarini, 
+// ularning sonini va yig’indisini chiqaruvchi dastur tuzing.
+//  let a = prompt(' a ni kiriting :')
+//  let = yigindi
+//  for(let  i = 1; i <= a; i++ ){
+// if (a % i == 0){
+//   yigindi += i;
+// }
+
+//  }
+// //  console.log(yigindi);
+
+// chiqmadi 
+
+
+
+
+
+
+
+
+
