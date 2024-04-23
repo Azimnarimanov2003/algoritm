15c187bf6646dbca52f81976b76a9afe5fd13abe
𝔹-𝕊𝕄𝔸ℝ𝕋 <azimnarimanov3@gmail.com>
2024-04-23 19:29:26 +0500

    algaritm

--- index.js
+++ index.js
@@ -419,11 +419,479 @@
 //  }
 // //  console.log(yigindi);
 
-// chiqmadi 
+// 1-misol
+// let a = prompt( ' a ni kiriting :')
+// let b = prompt(' b ni kiriting:')
+// let yigindi = 0;
+// let kupaytma = 1;
+// let ayirma = 0;
+//  for( let i = a; i <= b; i++){
+//   if( i % 2 == 0) {
+//     yigindi += i ;
+//     kupaytma += i * i ;
+//   }
+//   ayirma = ( yigindi-kupaytma)
+//  }
+// console.log(ayirma);
+// -94921
+
+// 2-misol
+// let a = prompt( ' a ni kiriting :')
+// let natija = 0
+// if( a > 0 && Math.sqrt(a)){
+// natija = Math.sqrt(a)
+// }
+// else{
+//   natija = ('bu sondan ildiz chiqmaydi')
+// }
+// console.log(natija);
+
+// 3-misol
+// let x=Number(prompt("x sonni kiriting"))
+// let summ=" "
+// let a=" "
+// for(i=1; i<=x; i++){
+//     summ+=i+a
+//     console.log(summ)
+// }
+  // 4-misol
+  // let n = prompt(' n ni kiriting :')-0
+  // let faktarial = 1;
+  // let yigindi = 0;
+  // for( let i = 1; i <= n; i ++ ){
+  //   faktarial *= i;
+  // yigindi += faktarial;
+  // }
+  // console.log(yigindi);
+
+  // 5-misol
+// let n = prompt('n ni kiriting: (n < 13)')
+// if( n > 0 && n < 4){
+//   console.log('qish');
+// }
+// else if(n > 3 && n < 7 ){
+//   console.log('bahor')
+// }
+// else if(n > 6 && n < 10 ){
+//   console.log('yoz')
+// }
+// else if(n > 9 && n < 13 ){
+//   console.log('kuz')
+// // }
+//  6-misol
+//  let  n = prompt(' n ni kirting:')
+//   let  sum = 0;
+//   let kupaytma = 1;
+//   for( let i = 1; i <= n; i++ ){
+//     kupaytma =i*i;
+// sum += kupaytma;
+//   }
+//   console.log(sum);
+
+// 7-misol
+//  let a = 37;
+//  let b = 127;
+//  let sum = 0;
+//  let yigindi = 0;
+//  for(let i = a; i < b; i++ ){
+//   if(i % 7 == 0 && i % 11 == 0){
+// sum += i;
+
+// yigindi =  Math.sqrt(sum)
+//   }
+// }
+//  console.log(yigindi);
+
+  // 9-misol
+//   let  a = prompt(' a ni kiriting :')-0
+//   let  b = prompt(' b ni kiriting :')-0
+//   let  c = prompt(' c ni kiriting :')-0
+//   let  d = prompt(' d ni kiriting :')-0
+//   let  oxirgisi = prompt(' oxirgisini  ni kiriting :')-0
+// let sum = 1;
+// let yigindi = 0;
+// sum = Math.sqrt((a * b * c * d * oxirgisi));
+// yigindi = (a+b+c+d+yigindi+sum)
+// if (Number.isInteger(sum)) {
+//   console.log("O'rta geometrigi:", sum);
+// } 
+// else
+//  {
+//   console.log("Yig'indi:", yigindi);
+// }
+
+// 10-misol
+// let a = prompt('a ni kiriting:')-0
+// let b = prompt('b ni kiriting:')-0
+//  let faktarial = 1;
+// for( let i = a; i <= b; i++){
+// faktarial *= i;
+// }
+// console.log(faktarial);
+
+// Func1. Tomoni a va b ga teng bo’lgan to’g’ri 
+// to’rtburchakning yuzasi va perimetrini hisoblovchi 
+// rectangle(a, b) nomli funksiya hosil qiling. QYM
+// Input: rectangle (6, 9)
+// Output: 54, 30
+
+// 1- misol
+// function rectangle(a, b) {
+//   p = 2*(a+b)
+//   s = a*b 
+//   return [p,s]
+// }
+// console.log(rectangle(1 ,5));
 
 
+// Func2. Tomoni a ga teng bo’lgan
+//  teng tomonli uchburchakning yuzasi va
+//   perimetrini hisoblovchi triangle(a) nomli funksiya hosil qiling. QYM
+// triangle (6)
+
+// 2-misol
+// function triangle(a){
+//   s= a*a*a
+//   p = 3*a
+//   return [s,p]
+// }
+// console.log(triangle(1));
+
+// Func3.  1 dan n ga sonlar yig’indisini 
+// qaytaruvchi sum(n) nomli funksiya hosil qiling. QY
+ 
+// 3-misol
+// function SumFunction(n){
+//   let sum = 0;
+//   for(let i = 1; i <= n; i ++){
+//     sum += i;
+//   }
+// return sum
+// }
+// console.log(SumFunction(2003));
+
+// isSquare (k) mantiqiy funksiyasini hosil qiling
+// . (k > 0). Agar k biror butun sonning kvadrati bo'lsa - true,
+//  aks holda false qiymat qaytarilsin. QY
+// isSquare(10) => false	
+// isSquare(25) => true
+// 4-misol
+
+// function  isSquare (k){
+//   if( Math.sqrt(k)%1===0){
+//   return true
+// }
+// else {
+//   return false
+// }
+// }
+// console.log(isSquare(49));
+
+// Butun qiymat qaytaruvchi digitCount(k)
+//  funksiyasini hosil qiling. (k > 0). Funksiya k 
+//  ning raqamlari sonini qaytarsin. QY
+// digitCount(8791) => 4
 
 
+//  5-misol
+
+
+// // Misol:
+// console.log(digitCount(8791)); // 4
+
+// function digitCount(k){
+//   let strK = k.toString();
+//   return strK.length;
+// }
+// console.log(digitCount(793544));
+
+
+// Func6. n natural sonning raqamlari yig'indisini
+//  hisoblovchi getDigitSum(n) nomli funksiya hosil qiling. QY
+// getDigitSum (456) => 15
+
+// 6-misol
+
+
+// UYGA VAZIFA
+// Func1. a sonning ixtiyoriy n-darajasini 
+// hisoblovchi power(a, n) nomli funksiya hosil qiling. QY
+// power(3, 5) => 243
+
+// 1-MISOL
+// function power(a, n){
+//   let daraja = 1;
+//   for(let i = 0; i < n; i++){
+// daraja *= a;
+//   }
+//   return daraja;
+
+// }
+// console.log(power(3, 5));
+// Func2. 2 ta a va b sonning o'rta 
+// arifmetigi (a + b) / 2 va geometrigi (a + b) ^ (1 / 2) 
+// hisoblovchi mean(a, b) nomli funksiya hosil qiling. QYM
+// mean(12, 48) => 30, 24
+// function  mean(a, b){
+//   A = (a+b)/2
+//   G = Math.sqrt(a+b)
+// return[ A, G]
+// }
+// console.log(mean(20, 5));
+
+
+// 3-misol
+// Func3. Haqiqiy sonning ishorasini aniqlovchi
+//  sign(n) nomli funksiya hosil qiling. Funksiya 
+//  argumenti noldan kichik bo'lsa -1; noldan katta bo'lsa 1;
+//   nolga teng bo'lsa 0 qiymat qaytarsin. QY
+// sign(10) => 1
+// function sign(n){
+//   if( n > 0){
+//     return 1
+//   }
+//   else if( n < 0){
+//     return -1
+//   }
+//   else {
+//     return 0
+//   }
+// }
+// console.log(sign(0));
+
+
+// A*x^2 + B*x + C = 0 ko'rinishidagi 
+// tenglama kvadrat tenglama deyiladi. 
+// (A noldan farqli son). Kvadrat tenglamaning ildizlar sonini 
+// aniqlovchi numberOfRoots(A, B, C) nomli funksiya hosil qiling.
+// D = B^2 - 4*A*C. Agar D > 0 bo’lsa 2 ta, D = 0 bo’lsa 1 ta, D < 0 bo’lsa 0 ta. QY
+// numberOfRoots (1, -6, 9) => 1
+
+//4-misol
+// function numberOfRoots(A, B, C){
+//   let D = B*B - 4*A*C;
+//   if(D > 0){
+//     return 2
+//   }
+//   else if(D === 0){
+//     return 1
+//   }
+//   else if(D < 0){
+//     return 0
+//   }
+// }
+// console.log(numberOfRoots(1,-6, 9));
+
+// Doiraning yuzini hisoblovchi areaCircle(R) nomli funksiya hosil
+//  qiling. Doiraning yuzi S = π*R^2 orqali hisoblanadi.
+
+// 5-misol
+// function areaCircle(R){
+//   S = 3.14*R*R
+//   return S
+// }
+// console.log(areaCircle(10));
+
+//6-misol 
+// Func6. A va B sonlari orasidagi sonlar 
+// yig'indisini hisoblovchi sumRange(A, B) nomli funksiya hosil qiling. 
+// Agar A > B bo'lsa, funksiya 0 qiymat qaytaradi. QY
+// sumRange(8, 10) => 27
+
+// function sumRange( a, b){
+  
+//   if( a > b){
+   
+//    return 0
+
+//   }
+//   let yigindi = 0;
+//    for(let i = a; i <= b; i++ ){
+// yigindi +=i
+// }
+// return yigindi
+// }
+// console.log(sumRange(90,92));
+
+
+//7-misol
+// Arifmetik amallarni bajaruvchi calc(A, B, S) funksiyasini hosil 
+// qiling. A va B haqiqiy sonlar. S o'zgaruchisi orqali bajariladigan
+//  arifmetik amal aniqlanadi. “-” – ayirish, “*” – ko'paytirish, “/” - bo'lish,
+//   “+” - qo'shish va boshqa belgilar uchun 0 qaytarsin. QY
+// calc(10, 15, “*”) => 150
+// calc(7, 8, “+”) => 15
+
+// function calc(A, B, S) {
+//   if(S=== '-'){
+//   return A-B
+//   }
+//   else if(S=== '+'){
+//     return A+B
+//   }
+//   else if(S=== '*'){
+//   return A*B
+//   }
+//   else if(S=== '/'){
+//   return A/B
+//   }
+//   else{
+//     return 0
+//   }
+// }
+// console.log(calc(7,3,'+'));
+
+//8-misol 
+// Func8. Butun sonning juft - toqligini aniqlovchi isEven(K) 
+// funksiyasini hosil qiling. Funksiya K juft son bo'lsa - true, 
+// aks xolda false qiymat qaytarsin. QY
+// isEven(10) => true
+
+// function isEven(K){
+//   if(K%2===0){
+//   return true
+//   }
+//   else{
+//     return false
+//   }
+// }
+// console.log(isEven(777777777777777777777777));
+
+//9-misol
+// Func9. Kiritilgan 3 ta a, b, c sonlarning eng kichigini,
+//  o’rtachasini va eng kattasini chiqaruvchi sortABC(a, b, c) nomli dastur tuzing. QYM
+// sortABC(10, 5, 8) => 5, 8, 10
+
+// function sortABC(a, b, c) {
+//   // Eng kichik, eng o'rtacha va eng katta qiymatlarni aniqlash
+//   let smallest = Math.min(a, b, c);
+//   let largest = Math.max(a, b, c);
+//   let middle = (a + b + c) - smallest - largest;
+
+//   // Natijani chiqarish
+//   return `${smallest}, ${middle}, ${largest}`;
+// }
+
+// // Misol:
+// console.log(sortABC(10, 90, 8)); // 5, 8, 10
+
+// 10-misol
+
+// isPowerN(K, N) mantiqiy funksiyasini hosil qiling. (K > 0). Agar K soni N soninig biror 
+// darajasi bo'lsa - true, aks xolda false qiymat qaytarilsin. QY
+
+// 11. isPrime(N) mantiqiy funksiyasini hosil
+//  qiling. (N > 0). Agar N soni tub bo'lsa - true, aks holda false qiymat qaytarilsin. QY
+// isPrime(7)  => true
+
+// function isPrime(N){
+//   for (let i = 2; i <= Math.sqrt(N); i++) {
+//     if (N % i === 0) {
+//         return false;
+//     }
+
+// else {
+// return true;
+// }
+// }
+// }
+// console.log(isPrime(97));  
+
+
+// function isPowerN(N){
+//   for( let i = 2; i <= Math.sqrt(N); i++ ){
+//     if(N % i === 0 ){
+//       return false
+//     }
+//     else{
+//       return true
+//     }
+//   }
+// }
+// console.log(isPowerN(7));
+
+//12-misol
+
+// javobi chiqmadi
+
+// isPrime funksiyasi orqali N gacha bo’lgan sonlar ichidan 
+// nechtasi tub ekanini aniqlovchi numberOfPrime(N) nomli dastur tuzilsin. QY
+// numberOfPrime(10) => 4
+
+// function numberOfPrime(N){
+//   let soni = 0;
+//   for( i = 2; i <= Math.sqrt(N); i++){
+//     if(N %  i === 0 ){
+// return false
+//     }
+//     else{
+//       true
+//     }
+//   }
+// }
+// function tubSon(N){
+// let soni = 0;
+// for(let i = 2; i <= N; i++){
+// if(numberOfPrime(i)){
+//   return soni ++;
+// }
+// else {
+//   return soni 
+// }
+// }
+
+// }
+// console.log(tubSon(12));
+// Func16. 1 dan N ga sonlar ko’paytmasini qaytaruvchi factorial(N) 
+// nomli funksiya hosil qiling. Agar N manfiy bo’lib qolsa, 1 qaytarilsin. QY
+// function factorial(N){
+//   if( N < 0){
+//     return 1
+//   }
+//   let kupaytma = 1;
+//   for( let i = 1; i <= N; i++){
+//     kupaytma *= i;
+    
+//   }
+//   return kupaytma
+// }
+// console.log(factorial(5));
+
+// 1 dan N bo’lgan sonlar ichida 3 bo’linadigan sonlar yig’indisini
+//  hisoblovchi getSum3(N) nomli dastur yozing.
+// getSum3(15) => 45
+
+// function getSum3(N){
+//   let yigindi = 0;
+  
+//   for(let i = 1; i <= N; i++){
+//     if(i % 3 === 0 ){
+//     yigindi += i;
+//   }
+// }
+// return yigindi;
+
+// }
+// console.log(getSum3(15));
+
+
+// 1 dan N ga sonlar bo’lgan juft va toqlar sonlar yig’indisini 
+// qaytaruvchi sumOddEven(N) nomli funksiya hosil qiling. QYM
+// sumOddEven(10) => 30, 25
+function sumOddEven(N) {
+  let juftSonYigindisi = 0;
+  let toqSonYigindisi = 0;
+  for(let i = 1;  i <= N; i++){
+if ( i % 2 === 0){
+  juftSonYigindisi += i;
+}
+else{
+  toqSonYigindisi += i;
+}
+}
+return [juftSonYigindisi, toqSonYigindisi];
+}
+console.log(sumOddEven(10));
 
 
 
